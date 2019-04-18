48c9a4547e6eb3fe348c30fa63b55ff0c88898c3
yohan2127 <zedamek@yahoo.com>
2019-04-18 10:55:09 -0400

    updated my jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: process.env.PASSWORD_SQL,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: process.env.PASSWORD_SQL,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
