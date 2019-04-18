0bf8cfe36cd061669de149b28268061a203e4e5f
yohan2127 <zedamek@yahoo.com>
2019-04-18 12:10:10 -0400

    my heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-if (process.env.JAWSDB_URL) {
+var connection;
+//make connection
+if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}else{
   connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: process.env.PASSWORD_SQL,
+    password: "Nathan192127",
     database: "burgers_db"
-  });
-};
-
-// Make connection.
+  })
+}
 connection.connect();
-
 // Export connection for our ORM to use.
 module.exports = connection;
